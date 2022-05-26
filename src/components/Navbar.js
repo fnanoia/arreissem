2cd299241168fc9ee015cfe5c16c30c59c387151
fnanoia <francisconanoia@hotmail.com>
2022-05-26 12:49:51 -0300

    navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,14 @@
+function Navbar(){
+    return (
+    <div className="menu">
+        <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Products</a></li>
+            <li><a href="#">About</a></li>
+            <li><a href="#">Contact</a></li>
+        </ul>
+    </div>
+    )
+};
+
+export default Navbar;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
+//importo libreria react
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
+//importo paquete ReactDOM
+import ReactDOM from 'react-dom/client';
+
+//importo el componente principal
+import App from './App';
+
+//importo css
+import './css/main.css'
+
+
+//crear un punto inicial donde la appa se va a renderizar
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+<React.StrictMode>
     <App />
-  </React.StrictMode>
-);
+  </React.StrictMode>);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
